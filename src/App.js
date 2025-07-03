b6e00b115c6d1aea32e0066276e883a4186d1d98
shakir258 <malik844594@gmail.com>
2025-07-04 00:29:56 +0530

    add routing

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Products from "./Products";
+import SingleProduct from "./SingleProduct";
+import Card from "./Cart";
+import ErrorPage from "./ErrorPage";
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/singleproduct/:id" element={<SingleProduct />} />
+        <Route path="/card" element={<Card />} />
+        <Route path="*" element={<ErrorPage />} />
+
+      </Routes>
+    </BrowserRouter>
+  )
+};
+
+export default App;
---
+++ src/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ErrorPage(props) {
+    return (
+        <div>
+            
+        ErrorPage
+        </div>
+    );
+}
+
+export default ErrorPage;
